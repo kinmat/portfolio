71bb99cd6b44c06fddbc5d46a2c4984102f14211
kinga-crey <kinga.matok@creygames.com>
2022-11-06 22:48:08 +0100

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-function App() {
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBnEC5t28RmQFQjp7O6qwXmPKnRQz9hx6U",
+  authDomain: "portfolio-fb75a.firebaseapp.com",
+  projectId: "portfolio-fb75a",
+  storageBucket: "portfolio-fb75a.appspot.com",
+  messagingSenderId: "1085725184440",
+  appId: "1:1085725184440:web:f66ca0e7026437b0f2bf4d",
+  measurementId: "G-MDCVRE9G0H",
+};
+
+// Initialize Firebase
+
+const App = () => {
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
   return (
     <div className="App">
       <header className="App-header">
@@ -20,6 +41,6 @@ function App() {
       </header>
     </div>
   );
-}
+};
 
 export default App;
